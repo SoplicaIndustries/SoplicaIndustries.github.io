ede03c3e757066d6385ccc2d64607d1bfc9799b7
Jack Soplica <82238300+SoplicaIndustries@users.noreply.github.com>
2023-01-19 22:14:36 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function age(){
     let birthDate = new Date('12/09/2005');
     let years = new Date(Date.now()-birthDate.getTime());
@@ -8,8 +9,17 @@ function age(){
 
 function show(id){
     let element = document.getElementById(id);
+    
     const elementRect = element.getBoundingClientRect();
     const absoluteElementTop = elementRect.top + window.pageYOffset;
-    const middle = absoluteElementTop - (window.innerHeight / 5);
+
+    var isMobile = Math.min(window.screen.width, window.screen.height) < 768 || navigator.userAgent.indexOf("Mobi") > -1;
+    let divider;
+    if(isMobile)divider=25; // set after setting margin of cards
+    else divider = 5;
+
+    console.log(isMobile + " " + divider)
+
+    const middle = absoluteElementTop - (window.innerHeight / divider);
     window.scrollTo({top: middle, behavior: 'smooth'});
 }
