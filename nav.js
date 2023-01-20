c7df180fb884c279d5632bbdf121590db313e8a2
Jack Soplica <82238300+SoplicaIndustries@users.noreply.github.com>
2023-01-20 23:34:28 +0100

    update

--- nav.js
+++ nav.js
@@ -2,11 +2,19 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   let nav = document.getElementById("nav")
+  let shadow = "nav-shadow"
 
   if (prevScrollpos > currentScrollPos) {
     nav.style.top = "0";
   } else {
-    nav.style.top = "-100px";
+    nav.style.top = "-200px";
+  }
+
+  if(window.scrollY==0){
+    if(nav.classList.contains(shadow)) nav.classList.remove(shadow)
+  }
+  else{
+    if(!nav.classList.contains(shadow)) nav.classList.add(shadow)
   }
   prevScrollpos = currentScrollPos;
 } 
