9319cdd86d992e7bcf6e1224043cdff1c3e03ac0
Jack Soplica <82238300+SoplicaIndustries@users.noreply.github.com>
2023-01-22 19:22:39 +0100

    mobile scroll fix

--- nav.js
+++ nav.js
@@ -4,10 +4,14 @@ window.onscroll = function() {
   let nav = document.getElementById("nav")
   let shadow = "nav-shadow"
 
+  console.log(window.scrollY)
+
   if (prevScrollpos > currentScrollPos) {
     nav.style.top = "0";
   } else {
+    if(window.scrollY > 150){
     nav.style.top = "-200px";
+    }
   }
 
   if(window.scrollY==0){
