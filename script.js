28aad18706704b88698bfdcc512d40cebc314a84
Jack Soplica <82238300+SoplicaIndustries@users.noreply.github.com>
2023-01-23 20:01:00 +0100

    mobile update 2

--- script.js
+++ script.js
@@ -31,14 +31,28 @@ function menuHandler(){
     let container = document.getElementById("nav-button-container");
     let style = container.style
     let dimm = document.getElementById("dimm")
+    let bar1 = document.getElementById("bar-top")
+    let bar2 = document.getElementById("bar-middle")
+    let bar3 = document.getElementById("bar-bottom")
 
     if(style.right == "-70%"){
 
         container.style.right = "0%"
         dimm.style.display = "block"
+        bar1.style.transform = "rotate(45deg)"
+        bar2.style.transform = "translateX(1rem)"
+        bar2.style.opacity ="0"
+        bar3.style.transform = "rotate(-45deg)"
+    }
+    else if(style.right == "0%"){
+        container.style.right = "-70%"
+        dimm.style.display = "none"
+        bar1.style.transform = "rotate(0deg)"
+        bar2.style.transform = "translateX(0rem)"
+        bar2.style.opacity ="1"
+        bar3.style.transform = "rotate(0deg)"
     }
     else{
         container.style.right = "-70%"
-        dimm.style.display = "none"
-    };
+    }
   }
